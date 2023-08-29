ff23c397dc5d9a59c91417e50bf5d42c8e4a8885
mrpaulishaili <paulishaili@gmail.com>
2023-08-29 05:41:42 +0100

    re-enabled server actions and removed millions.js from next.config

--- next.config.js
+++ next.config.js
@@ -1,15 +1,8 @@
 /** @type {import('next').NextConfig} */
-const million = require('million/compiler');
 const nextConfig = {
   experimental: {
     serverActions: true,
   },
 };
 
-const millionConfig = {
-  auto: true,
-  // if you're using RSC:
-  // auto: { rsc: true },
-};
-
-module.exports = million.next(millionConfig, nextConfig);
+module.exports = nextConfig;
