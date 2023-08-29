9bfa6d88018f84b3695fbab88a795163433d7375
mrpaulishaili <paulishaili@gmail.com>
2023-08-29 06:09:03 +0100

    .. tiny updates on header components

--- src/app/components/Buttons.component.tsx
+++ src/app/components/Buttons.component.tsx
@@ -6,7 +6,7 @@ import Link from 'next/link';
 export const LoginButton = () => {
   return (
     <button
-      className="user-login px-6 py-1 leading-tight md:py-2 text-sm md:text-base bg-yellow-700 text-gray-100 rounded-xl"
+      className="user-login px-6 py-2 leading-tight md:py-2 text-sm md:text-base bg-yellow-700 text-gray-100 rounded-xl"
       onClick={() => signIn()}
     >
       Sign in
--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -16,7 +16,7 @@ const Header: FC<HeaderProps> = ({}) => {
     <header className="sticky top-0 shadow-sm z-50 bg-white flex items-center px-4 md:px-24 h-max justify-between">
       {/* Search enabler */}
       <div
-        className="search-icon w-16 cursor-pointer hover:bg-gray-100 text-gray-900 px-6 py-3 rounded-xl transition-all m-0"
+        className="search-icon w-16 cursor-pointer hover:bg-gray-100 text-gray-900 py-3 rounded-xl transition-all m-0"
         onClick={() => setActive(!isActive)}
       >
         {isActive ? <FaTimes size={20} /> : <FaSearch size={20} />}
