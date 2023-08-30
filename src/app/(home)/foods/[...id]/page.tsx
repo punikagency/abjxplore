173c19dade3351039dd67f41ce2e1e2cfaa10689
mrpaulishaili <paulishaili@gmail.com>
2023-08-30 11:49:22 +0100

    nEW

--- src/app/(home)/foods/[...id]/page.tsx
+++ src/app/(home)/foods/[...id]/page.tsx
@@ -4,7 +4,11 @@ import { FC } from 'react';
 interface FoodShowPageProps {}
 
 const FoodShowPage: FC<FoodShowPageProps> = ({}) => {
-  return <MainContainer>FoodShowPage View</MainContainer>;
+  return (
+    <MainContainer>
+      <h1>FoodShowPage View</h1>
+    </MainContainer>
+  );
 };
 
 export default FoodShowPage;
